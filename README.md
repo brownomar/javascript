ca8c7ca2437381a9fe3027038c28e5d02b93f05d
Omar <brownomar@hotmail.com>
2025-09-05 21:12:47 -0500

    fixed the span thing

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Beginning Web Programming class work
+creating a basic website that uses Javascript and JQuery
--- userInfo.js
+++ userInfo.js
@@ -2,4 +2,4 @@ var input=prompt("Enter your name:");
 document.write("Hello "+input+"!");
 document.write("<br>");
 var input2=prompt("Please enter your age:");
-document.write(input+"You are "+input2+" years old.");
+document.write("Hi "+input+", you are "+input2+" years old.");
