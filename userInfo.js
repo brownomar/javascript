83c3b4ea5dec8ebdc697ed308671056de73f9d85
Omar <brownomar@hotmail.com>
2025-09-07 17:23:57 -0500

    added side nav bar and sticky top nav bar, changed background colors, added alert to userInfo.js

--- userInfo.js
+++ userInfo.js
@@ -1,3 +1,3 @@
 var input=prompt("Enter your name:");
-alert("Welcome to the site, "+input+"!");
-document.write("<h2>Welcome, "+input+"!</h2>");W
+alert("Thanks, "+input+"!");
+document.write("<h1>Welcome, "+input+"!</h1>");
