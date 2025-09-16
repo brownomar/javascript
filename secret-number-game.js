64564cc1b4ab7b3c98848500ee0602af309f2b22
Omar <brownomar@hotmail.com>
2025-09-15 23:12:09 -0500

    created a number guessing game using JavaScript. Also made some adjustments to the CSS for better spacing on smaller screens on quotes

---
+++ secret-number-game.js
@@ -0,0 +1,20 @@
+let secretNumber = 4;
+let userScore = 0;
+let guessNumber = 0;
+// Input validation to ensure user enters a number between 1 and 5
+while (guessNumber < 1 || guessNumber > 5 || isNaN(guessNumber)) {
+let guessNumber = prompt("Guess a number between 1 and 5. Make sure its a number!" );
+}
+
+// Main game loop - continues until userScore reaches 10
+while (userScore < 10) {
+if (Number(guessNumber) === secretNumber) {
+    alert("Congratulations! You guessed the secret number!");
+    userScore += 10;
+    alert("Your score is: " + userScore+" points. You win!");
+} else {
+    alert("Sorry, that was not the secret number. Try again!");
+}
+}
+document.write("The secret number was: " + secretNumber + "<br>");
+document.write("Your final score is: " + userScore + " points.<br>");
