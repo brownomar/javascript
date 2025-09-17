e5fbecab5dd6294a83a20c5a0373e18088e62e97
Omar <brownomar@hotmail.com>
2025-09-17 02:33:53 -0500

    declaration fixed

--- secret-number-game.js
+++ secret-number-game.js
@@ -10,10 +10,8 @@ if (guessNumber < 1 || guessNumber > 5 || isNaN(guessNumber)) {
     continue; // Prompt the user again
     }
     else if (guessNumber == secretNumber) {
-        
+        let userScore = userScore + 10;
         document.write("Congratulations! The secret number was: " + secretNumber + "!<br>");
-        userScore = userScore + 10;
-        
         document.write("Your final score is: " + userScore + " points.<br> Game Over, Man!");
         break; // Exit the loop if the guess is correct
     }
