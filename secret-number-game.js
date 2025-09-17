f2b316f7925889d73f13ded5a8918e36bf29776c
Omar <brownomar@hotmail.com>
2025-09-16 22:29:24 -0500

    Hopefully no endless loop this time!

--- secret-number-game.js
+++ secret-number-game.js
@@ -1,8 +1,17 @@
 let secretNumber = 4;
 let userScore = 0;
-
-let guessNumber = prompt("Guess a number between 1 and 5. Make sure its a number 1 - 5!" );
-document.write("You guessed: " + guessNumber + "<br>");
-
-document.write("The secret number was: " + secretNumber + "<br>");
-document.write("Your final score is: " + userScore + " points.<br>");
+var guessNumber;
+// Loop until the user guesses the correct number
+while (guessNumber != secretNumber) {
+    let guessNumber = prompt("Guess my favorite number between 1 and 5. Make sure its a number 1 - 5!" );
+if (guessNumber < 1 || guessNumber > 5 || isNaN(guessNumber)) {
+    alert("Please enter a valid number between 1 and 5.");
+    }
+document.write("You guessed: " + guessNumber + " ...which was wrong.<br>");
+}
+// User guessed the correct number
+if (guessNumber == secretNumber) {
+    userScore = userScore + 10;
+document.write("Congratulations! The secret number was: " + secretNumber + "!<br>");
+document.write("Your final score is: " + userScore + " points.<br> Game Over, Man! Game Over!");
+}
