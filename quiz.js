232dbc0ac8ad57ea960200c17b0f03e221efe851
Omar <brownomar@hotmail.com>
2025-09-23 17:15:45 -0500

    initializing quiz game setup

---
+++ quiz.js
@@ -0,0 +1,13 @@
+let question1 = "What is the answer to question 1?";
+let question2 = "What is the answer to question 2?";
+let question3 = "What is the answer to question 3?";
+let question4 = "What is the answer to question 4?";
+let answer1 = "Answer 1";
+let answer2 = "Answer 2";
+let answer3 = "Answer 3";
+let answer4 = "Answer 4";
+
+var questionArray = [question1, question2, question3, question4];
+var answerArray = [answer1, answer2, answer3, answer4];
+
+for (questionArray)
