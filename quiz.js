87ec00695b7a4107057c71786d56f3b5786b43a4
Omar <brownomar@hotmail.com>
2025-09-23 23:18:05 -0500

    writing answers to the webpage

--- quiz.js
+++ quiz.js
@@ -14,9 +14,11 @@ for (let x = 0; x < questionArray.length; x++) {
 	// Q&A time
     let userAnswer = prompt(questionArray[x]);
     if (userAnswer.toLowerCase() === answerArray[x].toLowerCase()) {
+        document.write("You answered "+ userAnswer+", which was correct!");
         alert ("That is correct!");
     }
     else {
+        document.write("You answered " + userAnswer + " which was wrong.");
         alert ("No. Try again.");
         x-=1;
     }
