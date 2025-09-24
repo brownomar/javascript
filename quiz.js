46bc91785db2e75a9583b1791db99070e63701a0
Omar <brownomar@hotmail.com>
2025-09-23 23:25:29 -0500

    adding quotes and bolding answers in log

--- quiz.js
+++ quiz.js
@@ -14,11 +14,11 @@ for (let x = 0; x < questionArray.length; x++) {
 	// Q&A time
     let userAnswer = prompt(questionArray[x]);
     if (userAnswer.toLowerCase() === answerArray[x].toLowerCase()) {
-        document.write("You answered "+ userAnswer+", which was correct!<br>");
+        document.write("You answered <strong>\'"+ userAnswer+"\'</strong>, which was correct!<br>");
         alert ("That is correct!");
     }
     else {
-        document.write("You answered " + userAnswer + " which was wrong.<br>");
+        document.write("You answered <strong>\'" + userAnswer + "\'</strong> which was wrong.<br>");
         alert ("No. Try again.");
         x-=1;
     }
