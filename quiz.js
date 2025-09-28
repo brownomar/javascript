3ce1233d68393efb0db081f649842418cbc7aab4
Omar <brownomar@hotmail.com>
2025-09-28 00:13:42 -0500

    formatting final results on page

--- quiz.js
+++ quiz.js
@@ -36,14 +36,14 @@ function tryExceeded () {
 
 
 //display questions and answers
-document.write("<h3>Here are the questions and answers:</h3><br>");
+document.write("<h3>Here are the questions and answers:</h3>");
 for (let i=0; i<questionArray.length; i++) {
     document.write("Question "+(i+1)+": "+questionArray[i]+"<br>");
     document.write("Answer: "+answerArray[i]+"<br>");
 }
 
 // user's activity log
-document.write("<h3>Your Quiz Results:</h3><br>");
+document.write("<h3>Your Quiz Results:</h3>");
 
 
 //loop counter variable
@@ -84,7 +84,7 @@ while (x < questionArray.length) {
     }
     }
     //final score display
-    document.write("<h2><strong>Your final score is "+userScore+" out of "+(pointsAwarded.length*answerArray.length)+" points.</strong></h2><br><br> <a href=\"quiz.html\">Refresh</a> to go again!<br>");
+    document.write("<h2><strong>Your final score is "+userScore+" out of "+(pointsAwarded.length*answerArray.length)+" points.</strong></h2><a href=\"quiz.html\">Refresh</a> to go again!<br>");
         
 
  
