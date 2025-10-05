1b101d4d85f4b0a6e06858e152cf277a58cfe4e9
Omar <brownomar@hotmail.com>
2025-10-05 09:55:23 -0500

    bolded daily quote

--- trivia.js
+++ trivia.js
@@ -35,7 +35,7 @@ document.write("Good "+timeOfDay+". Today is " + days[dayOfWeek] + ", " + months
 //quote array and selector
 let fiveQuotes = ["Like a ten-speed bike, most of us have gears we do not use.", "When spiderwebs unite, they can tie up a lion.","Reading is to the mind what exercise is to the body.", "You can't win unless you know how to lose.", "You cannot shake hands with a clenched fist."];
 let dailyQuote = fiveQuotes[Math.floor(Math.random()*4)];
-document.write("Here is your quote for the day:<br><em>\""+dailyQuote+"\"</em><br><br>");
+document.write("Here is your quote for the day:<br><strong><em>\""+dailyQuote+"\"</em></strong><br><br>");
 
 
 //old quiz code below
