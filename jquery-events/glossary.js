b72ccff053b95ad6e7f6021913b59369fadd23ec
Omar <brownomar@hotmail.com>
2025-10-28 23:11:58 -0500

    first try at displaying image on mouseover event

--- jquery-events/glossary.js
+++ jquery-events/glossary.js
@@ -2,6 +2,24 @@
 $(document).ready(function() {
     //hide botanic names on page load
     $(".botanic").hide();
+    //hide the imgdivs on page load
+    $(".imgdiv").hide();
+    //mouseover and mouseout to show/hide images for pic spans
+    //get attribute and concatenate # with alt attribute value
+    var imgID = "#" + $(this).attr("alt");
+    //put x y coordinates of event in variable
+    $(".pic").mouseover(function(event) {
+        var x = event.pageX + 150;
+        var y = event.pageY;
+        //set css position of imgdivs to x and y coordinates
+        $(imgID).css({top: y + "px", left: x + "px"});
+        //show the imgdiv
+        $(imgID).show();
+    });
+    $(".pic").mouseout(function() {
+        //hide the imgdiv
+        $(imgID).hide();
+    });
     //hovering over flower names changes cursor to pointer
     $(".flower").css("cursor", "pointer");
     //hide any displayed botanic names when flower clicked
