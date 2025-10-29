2bdd24f613341a8d873772f4158564b71fbc17ff
Omar <brownomar@hotmail.com>
2025-10-28 21:58:22 -0500

    hide botanical names and show common names when clicked.

--- jquery-events/glossary.js
+++ jquery-events/glossary.js
@@ -1,5 +1,15 @@
 //page load function
 $(document).ready(function() {
+    //hide bonanic names until flower id is clicked
+    $(".botanic").hide();
+    //first hide any displayed botanic names when flower id is clicked
+    $(".flower").click(function() {
+        $(".botanic").hide();
+    });
+    //display botanic names when flower id is clicked
+    $(".flower").click(function() {
+        $(this).children(".botanic").show();
+    });
     //mouseover and mouseout changes h1 and h2 colors
     $("h1").mouseover(function() {
         $(this).css("color", "red");
@@ -13,10 +23,6 @@ $(document).ready(function() {
     $("h2").mouseout(function() {
         $(this).css("color", "black");
     });
-    //hide bonanic names until flower id is clicked
-    $(".botanic").hide();
-    $(".flower").click(function() {
-        $(this).next(".botanic").toggle();
-    });
+    
 
 });
