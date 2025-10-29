fffdf509deaaf8f414553cdcc39231c27c07b933
Omar <brownomar@hotmail.com>
2025-10-28 21:44:55 -0500

    hide botanial names until flower name is clicked

--- jquery-events/glossary.js
+++ jquery-events/glossary.js
@@ -13,31 +13,10 @@ $(document).ready(function() {
     $("h2").mouseout(function() {
         $(this).css("color", "black");
     });
-    //hide botanic names and display individual ones when flower clicked
-    $("dd").hide();
-    $("dt").click(function() {
-        $(this).next("dd").toggle();
-    });
-    //hover displays images for select flowers beside flower name/ hides on mouseout
-    $("#rose").hover(
-        function() {
-            $("#roseImg").show();
-        },
-        function() {
-            $("#roseImg").hide();
-        }
-    );
-    //keypress moves to first flower fore ah letter when letter key pressed
-    $(document).keypress(function(event) {
-        var key = String.fromCharCode(event.which).toLowerCase();
-        $("dt").each(function() {
-            if ($(this).text().charAt(0).toLowerCase() === key) {
-                $('html, body').animate({
-                    scrollTop: $(this).offset().top
-                }, 500);
-                return false; // exit each loop
-            }
-        });
+    //hide bonanic names until flower id is clicked
+    $(".botanic").hide();
+    $(".flower").click(function() {
+        $(this).next(".botanic").toggle();
     });
 
 });
