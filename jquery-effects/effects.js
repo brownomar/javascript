8436d8031ad90a8aba1a48504c5f9a5adcaaac52
Omar <brownomar@hotmail.com>
2025-11-02 22:52:23 -0600

    ok jeez. had parenthesis in the wrong place for the callback function

--- jquery-effects/effects.js
+++ jquery-effects/effects.js
@@ -1,20 +1,41 @@
 // to show/hide newsSignup form
 $(document).ready(function(){
-    //hide form at startup
-    $("#newsSignup").hide();
+    //hide form at page load
+    $('#newsSignup').hide();
     //click linke and toggle form
-    $("#signuplink").click(function(event){
+    $('#signuplink').click(function(event){
         //prevent default action of the link
         event.preventDefault();
         //toggle the form
-        $("#newsSignup").slideToggle(500);
+        $('#newsSignup').slideToggle("slow");
         //open and close form with +/-
-        var signuplink = $("#openclose").text();
+        var signuplink = $('#openclose').text();
         //toggle + and -
         if (signuplink === "+") {
-            $("#openclose").text("-");
+            $('#openclose').text("-");
         } else {
-            $("#openclose").text("+");
+            $('#openclose').text("+");
         }
-  });
+    });
+    //make mouse cursor a pointer when over slogan
+    $('#slogan').css('cursor', 'pointer');
+    //mouseover event for slogan element
+    $('#slogan').mouseover(function(){
+        //fade out slogan element normal/linear
+        $('#slogan').fadeOut("normal", "linear", function(){
+        //fade to new slogan slow/swing
+        $('#slogan').text("Hand Picked Just for You").fadeIn("slow", "swing");
+        });
+    });
+
+    $('#slogan').mouseout(function(){
+        //fade slogan out at fast speed with swing
+        $('#slogan').fadeOut("fast", "swing", function(){
+        //change text back to original slowly with linear easing
+        $('#slogan').text("The Power of Flowers").fadeIn("slow", "linear");
+        });
+    });
+    
+
+
 });
