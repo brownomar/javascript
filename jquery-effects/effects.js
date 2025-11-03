d9466e4a9df3d706093e124dbcaa17c6c589e5a7
Omar <brownomar@hotmail.com>
2025-11-02 19:58:02 -0600

    hide form, clicking toggles form, and changes + to –

--- jquery-effects/effects.js
+++ jquery-effects/effects.js
@@ -1,6 +1,18 @@
-//use slide toggle to show/hide newsSignup form
+// to show/hide newsSignup form
 $(document).ready(function(){
-  $("#newsToggle").click(function(){
-    $("#newsSignup").slideToggle("slow");
+    //hide form at startup
+    $("#newsSignup").hide();
+    //click linke and toggle form
+    $("#signuplink").click(function(event){
+        //prevent default action of the link
+        event.preventDefault();
+        //open and close form with +/-
+        var signuplink = $("#openclose").text();
+        //toggle + and -
+        if (signuplink === "+") {
+            $("#openclose").text("-");
+        } else {
+            $("#openclose").text("+");
+        }
   });
 });
