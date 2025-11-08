002eb0aba9b1f8978e930cfd0f1e830688cf1db8
Omar <brownomar@hotmail.com>
2025-11-08 17:10:53 -0600

    remembered html ready function, added event handlers for the buttons, and updated the cart total when items are added.

--- jquery-dom/shopping.js
+++ jquery-dom/shopping.js
@@ -1,19 +1,26 @@
-//variable to count number of items in cart
-let itemCount = 0;
-//change cursor to pointer for starts and add to cart elements
-$('.rating span, .add').css('cursor', 'pointer');
-//variable to hold html for creating a delete button 
-const deleteButtonHTML = '<span class="delete">X</span>';
-//add click event listener to add to cart buttons
-$('.add').on('click', function() {
-  //increment item count
-    itemCount++;
-    //get the name of the item to add to cart
-    const itemName = $(this).siblings('h3').text();
-    //create a new list item with the item name and delete button
-    const listItemHTML = `<li>${itemName} ${deleteButtonHTML}</li>`;
-    //append the new list item to the cart
-    $('#cart ul').append(listItemHTML);
-    //update the item count display
-    $('#itemCount').text(itemCount);
+//wait for page to load
+$(document).ready(function() {
+    //variable to count number of items in cart
+    let itemCount = 0;
+    //change cursor to pointer for starts and add to cart elements on hover
+    $('.rating, .add').css('cursor', 'pointer');
+    //variable to hold html for creating a delete button 
+    const deleteButtonHTML = '<span class="delete">X</span>';
+    //add click event listener to add to cart buttons
+    $('.add').on('click', function() {
+    //increment item count
+        itemCount++;
+        //get the name of the item to add to cart use name attribute
+        const itemName = $(this).attr('name');
+        //cartLink variable to hold the html for the new cart item
+        var cartLink = "<li class='cartItem' name='" + itemID + "'>" + itemName + " <span class='del'>Remove</span></li>";
+        //append list item to end of cart ul
+        $('#cart ul').append(cartLink);
+        //hide add cart button for this item
+        $(this).hide();
+        //update item count in cart display
+        $('#itemCount').text(itemCount);
+        
+    });
+
 });
