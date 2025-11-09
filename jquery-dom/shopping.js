e7d9d72580f6990506be0bfe2e787b93e62620de
Omar <brownomar@hotmail.com>
2025-11-08 22:49:55 -0600

    fixed shopping cart adding and removing display issues

--- jquery-dom/shopping.js
+++ jquery-dom/shopping.js
@@ -1,26 +1,47 @@
 //wait for page to load
+
 $(document).ready(function() {
     //variable to count number of items in cart
     let itemCount = 0;
-    //change cursor to pointer for starts and add to cart elements on hover
-    $('.rating, .add').css('cursor', 'pointer');
-    //variable to hold html for creating a delete button 
-    const deleteButtonHTML = '<span class="delete">X</span>';
+    
     //add click event listener to add to cart buttons
     $('.add').on('click', function() {
-    //increment item count
+        
+        //create itemcount element in the shopping cart when first item is added
+        if (itemCount === 0) {
+            $('#cart').prepend("<p>Your Cart (<span id='itemCount'>0</span> items)</p><ul></ul>");
+            //hide empty cart message
+            $('#empty').hide();
+        }
+        //increment item count
         itemCount++;
+
+        //get the id of the item to add to cart use id attribute
+        const itemID = $(this).attr('id');
         //get the name of the item to add to cart use name attribute
         const itemName = $(this).attr('name');
         //cartLink variable to hold the html for the new cart item
         var cartLink = "<li class='cartItem' name='" + itemID + "'>" + itemName + " <span class='del'>Remove</span></li>";
-        //append list item to end of cart ul
+        
+        //append list item to end of cart
         $('#cart ul').append(cartLink);
-        //hide add cart button for this item
-        $(this).hide();
         //update item count in cart display
         $('#itemCount').text(itemCount);
-        
     });
-
+    //delegated event listener for delete buttons
+    $('#cart').on('click', '.del', function() {
+        //remove the parent li of the clicked delete button
+        $(this).parent().remove();
+        //decrement item count
+        itemCount--;
+        //update item count in cart display
+        $('#itemCount').text(itemCount);
+         //if item count is 0 show empty cart message and remove item count element
+        if (itemCount < 1) {
+            $('#empty').show();
+            $('#cart p').remove();
+            $('#cart ul').remove();
+        }
+    });
 });
+
