7b4f0d135242332f321d23238ab30b4b5a040826
Omar <brownomar@hotmail.com>
2025-11-15 15:10:02 -0600

    open window with image from clicking lgPic

--- jquery-images/gallery.js
+++ jquery-images/gallery.js
@@ -1,10 +1,26 @@
 //wait for load
 $(document).ready(function(){
 //variable for image name
-var displayImg = $('thumbs').mouseover(attr).text;
+var displayImg = '';
 //hover effect
-$('thumbs').mouseover(function(event){
-    $('figure').attr('src', displayImg);
-    })
+$('#thumbs').mouseover(function(event) {
+    //add thin dark green border on hovear
+    $(this).css('border', '2px solid darkgreen');
+    }).mouseout(function() {
+        $(this).css('border', 'none');
+    });
+
+//click each smaller image to change larger image
+$('#thumbs img').click(function() {
+    //getr src of clicked thumb
+    displayImg = $(this).attr('src');
+    //set src to large image
+    $('#lgPic').attr('src', displayImg);
+});
+
+//click large image to open in the image in new window
+$('lgPic').click(function() {
+    window.open(this).attr('src', '_blank');
+});
 
 });
