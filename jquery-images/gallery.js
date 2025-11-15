ed6e58b3a79e9a4d26acfdd923d1d0e45d7ca9ee
Omar <brownomar@hotmail.com>
2025-11-15 15:04:32 -0600

    added click thumbs to change large pic

--- jquery-images/gallery.js
+++ jquery-images/gallery.js
@@ -1,6 +1,10 @@
-//ready
-$(document).ready(function (){
-
-
+//wait for load
+$(document).ready(function(){
+//variable for image name
+var displayImg = $('thumbs').mouseover(attr).text;
+//hover effect
+$('thumbs').mouseover(function(event){
+    $('figure').attr('src', displayImg);
+    })
 
 });
