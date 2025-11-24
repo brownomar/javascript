480f2b7b3777acac3c1dc0714f7fa2862fa6b76e
Omar <brownomar@hotmail.com>
2025-11-24 11:19:57 -0600

    copied working validation for name and created email validation replacing name with email

--- jquery-orderform/orderform.js
+++ jquery-orderform/orderform.js
@@ -4,33 +4,37 @@ console.log("ready");
 //Fly, you fools!
     //put cursor in name field
     $('#name').focus();
-    //$('#name').val('test');
-    console.log('test');
 
     //name field required
     $('#name').blur(function(){
         var nameVal = $(this).val();
         if (nameVal === ''){
             $('#nameErr').text('Name is required');
-        }else{
+        }
+        else{
             $('#nameErr').text('');
         } 
     });
-    // //email required and valid
-    // $('#email').blur(function(){
-    // var emailVal = $(this).val();    
-        
-    //     if (emailVal ===''){
-    //         $('#emailErr').val('Email is required');
-    //         }
-    //     else{
-    //         $('#emailErr').val('');
-    //         var emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-    //         if (emailRegex.val(emailVal)){
-    //             $(this).val('');
-    //         }else{
-    //             $(this).val('Invalid email');
-    //         }
-    //     }
-    // })
+    $('#email').focus();
+    //email required and valid
+   $('#email').blur(function(){
+        var emailVal = $(this).val();
+        if (emailVal === ''){
+            $('#emailErr').text('Email is required');
+        }
+        else{
+            $('#emailErr').text('');
+        } 
+    });
+
+
+
+            // var emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+            // if (emailRegex.val(emailVal)){
+            //     $(this).text('');
+            // }else{
+            //     $(this).text('Invalid email');
+            // }
+       
+
 });
