0ae9d5d487b414980eea4d84952018c7d55bdda3
Omar <brownomar@hotmail.com>
2025-11-24 17:47:01 -0600

    address, city, zip

--- jquery-orderform/orderform.js
+++ jquery-orderform/orderform.js
@@ -9,28 +9,56 @@ console.log("ready");
     $('#name').blur(function(){
         var nameVal = $(this).val();
         if (nameVal === ''){
-            $('#nameErr').text('Name is required');
+            $('#nameErr').text('Name is required,');
         }
         else{
             $('#nameErr').text('');
         } 
     });
-    
+    //address field required
+    $('#address').blur(function(){
+        var addressVal = $(this).val();
+        if (addressVal === ''){
+            $('#addressErr').text('Address is required.');
+        }
+        else{
+            $('#addressErr').text('');
+        } 
+    });
+    //city field required
+     $('#city').blur(function(){
+        var cityVal = $(this).val();
+        if (cityVal === ''){
+            $('#cityErr').text('City is required.');
+        }
+        else{
+            $('#cityErr').text('');
+        } 
+    });
+    //state field required
+     $('#state').blur(function(){
+        var stateVal = $(this).val();
+        if (stateVal === ''){
+            $('#stateErr').text('State is required');
+        }
+        else{
+            $('#stateErr').text('');
+        } 
+    });
     //email required and valid
    $('#email').blur(function(){
         var emailVal = $(this).val();
         if (emailVal === ''){
-            $('#emailErr').text('Email is required');
+            $('#emailErr').text('Email is required.');
         }
         else{
             var emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
             if (emailRegex.test(emailVal)){
                 $('#emailErr').text('');
             }else{
-                $('#emailErr').text('Invalid email');
+                $('#emailErr').text('Invalid email.');
             }
-        }
-         
+        }      
     });
 
 
