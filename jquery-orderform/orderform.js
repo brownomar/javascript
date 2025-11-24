bc5d4507c7663497c0c0155eefa07f90be7d649f
Omar <brownomar@hotmail.com>
2025-11-23 18:21:05 -0600

    putting cursor in name field and pressing tab not working :()

--- jquery-orderform/orderform.js
+++ jquery-orderform/orderform.js
@@ -2,32 +2,33 @@
 $(document).ready(function(){
 console.log("ready");
 //Fly, you fools!
-//put cursor in name field
-$('#name').focus();
-//name field required
-$('#name').blur(function(){
-    var nameVal = $('#name').val();
-    if (nameVal = ''){
-        $('#nameError').text('Name is required');
-    }else{
-        $('#nameError').text('');
-    }
-});
-//email required and valid
-$('#email').blur(function(){
-    var emailVal = $('#email').val();
-    
-    if (emailVal = ''){
-        $('#emailError').text('Email is required');
-        }
-    else{
-        $('#emailError').text('');
-        var emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-        if (emailRegex.test(emailVal)){
-            $('#emailError').text('');
+    //put cursor in name field
+    $('#name').focus();
+
+    //name field required
+    $('#name').blur(function(){
+        var nameVal = $('#name').val();
+        if (nameVal = ''){
+            $('#nameError').text('Name is required');
         }else{
-            $('#emailError').text('Invalid email');
+            $('#nameError').text('');
+        } 
+    });
+    //email required and valid
+    $('#email').blur(function(){
+        var emailVal = $('#email').val();
+        
+        if (emailVal = ''){
+            $('#emailError').text('Email is required');
+            }
+        else{
+            $('#emailError').text('');
+            var emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+            if (emailRegex.test(emailVal)){
+                $('#emailError').text('');
+            }else{
+                $('#emailError').text('Invalid email');
+            }
         }
-    }
-})
+    })
 });
